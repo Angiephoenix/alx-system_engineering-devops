580a263345d8eb3c4818312dce99d59cc450ab9a
Angiephoenix <angiephoenix93@gmail.com>
2022-11-10 10:31:42 -0800

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# Test how web server setup featuring Nginx is doing under pressure
+# Testing how web server setup featuring Nginx is doing under pressure
 exec {'24: Too many open files':
   command => 'sed -i "s/15/4098/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
